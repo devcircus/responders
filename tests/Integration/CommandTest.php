35d3e5d6eda0a4b1abd7158f03d38358ef9d2ac1
Clayton Stone <devcircus@outlook.com>
2018-07-10 12:20:07 -0500

    Add Command test. Rename command namespace to 'bright'.

---
+++ tests/Integration/CommandTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BrightComponents\Responder\Tests\Integration;
+
+use Artisan;
+
+class CommandTest extends IntegrationTestCase
+{
+    /** @test */
+    public function bright_responder_command_makes_responder_with_correct_respond_method()
+    {
+        Artisan::call('bright:responder', ['name' => 'MyResponder']);
+
+        include_once base_path().'/app/Http/Responders/MyResponder.php';
+
+        $this->assertInstanceOf(\BrightComponents\Responder\Responder::class, $this->app[\App\Http\Responders\MyResponder::class]);
+        $this->assertMethodExists(\App\Http\Responders\MyResponder::class, config('responders.method'));
+    }
+
+    public function assertMethodExists(string $className, string $methodName)
+    {
+        $this->assertTrue(method_exists($className, $methodName));
+    }
+}
